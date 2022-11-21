8e06503511e04b10f1e6030b684d3825824cabb6
Rosario Nici <rosarionici95@gmail.com>
2022-11-21 10:22:01 +0100

    frist commit of project

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view("guest.home");
+})->name('index');
+
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('/', 'HomeController@index')->name('index');
+        Route::resource('posts', 'PostController');
+    });
+
+
+Route::get("{any?}", function () {
+    return view("guest.home");
+})->where("any", ".*");
