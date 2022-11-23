05cd36e81c5b12f71e77f3aaa1a35d0b40bf79d4
Rosario Nici <rosarionici95@gmail.com>
2022-11-23 16:03:57 +0100

    end frist part of boolpress

--- public/js/front.js
+++ public/js/front.js
@@ -50523,8 +50523,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\MAMP\bin\php\laravel-api\resources\js\front.js */"./resources/js/front.js");
-module.exports = __webpack_require__(/*! C:\MAMP\bin\php\laravel-api\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\MAMP\bin\php\laravel-boolpress\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\MAMP\bin\php\laravel-boolpress\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
